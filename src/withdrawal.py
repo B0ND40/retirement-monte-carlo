5453fb7c12d38cfbe4f6a50d7b7c4ed108b2d723
Daniel Bond <dbond6@vols.utk.edu>
2026-04-09 14:55:28 -0400

    Add mortality, withdrawal, and simulation modules

---
+++ src/withdrawal.py
@@ -0,0 +1,15 @@
+def fixed_withdrawal(balance, withdrawal_amount):
+	"""
+	Subtract a fixed withdrawal amount from the portfolio.
+
+	Parameters:
+		balance (float): current portfolio value
+		withdrawal_amount (float): annual withdrawal
+
+	Returns:
+		float: updated balance
+	"""
+	return balance - withdrawal_amount
+
+if __name__ == "__main__":
+	print(fixed_withdrawal(100000, 40000))
